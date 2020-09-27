f85add40a3d61e7fdd9332e24aff245e3f811cd2
Rumesh Aponso <rumesh.aponso@ifsworld.com>
2020-09-27 21:58:02 +0530

    ios, safari issue fixed

--- cv/js/core.js
+++ cv/js/core.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    // document.getElementById("btn_back_to_top").style.display = "none";
+document.addEventListener("DOMContentLoaded", function(event) {
     $("#btn_back_to_top").hide();
+
+    $(".wrapper").scroll(function(){
+        let scroll = $(".wrapper").scrollTop();
+        
+        if (scroll > 0) {
+            $("#btn_back_to_top").fadeIn();
+        } else {
+            $("#btn_back_to_top").fadeOut();
+        }
+    });
 });
 
-window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
 
-    if (scroll > 0) {
-        $("#btn_back_to_top").fadeIn();
-        // document.getElementById("btn_back_to_top").style.display = "block";
-    } else {
-        $("#btn_back_to_top").fadeOut();
-    }
-});
