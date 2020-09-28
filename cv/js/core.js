5aa49e35b615a7306c7125431b3a2fd8fa4cd0a9
Rumesh Aponso <rumesh.aponso@ifsworld.com>
2020-09-28 15:27:18 +0530

    Update core.js

--- cv/js/core.js
+++ cv/js/core.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    $("#btn_back_to_top").hide();
+    let scroll = $(".wrapper").scrollTop();
+        
+    if (scroll > 0) {
+        $("#btn_back_to_top").fadeIn();
+    } else {
+        $("#btn_back_to_top").fadeOut();
+    }
 
     $(".wrapper").scroll(function(){
         let scroll = $(".wrapper").scrollTop();
