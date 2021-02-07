0af983157ca9d28959e753aa094cf282451b5754
Rumesh Aponso <rumesh.aponso@ifsworld.com>
2021-02-07 15:11:03 +0530

    issue fixed

--- cv/js/core.js
+++ cv/js/core.js
@@ -4,6 +4,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $(".wrapper").scroll(function(){
         toggleBackToTop();
     });
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        //panel.style.display = "none";
+        $(panel).slideUp();
+        } else {
+        //panel.style.display = "block";
+        $(panel).slideDown();
+        }
+    });
+    }
 });
 
 function toggleBackToTop() {
@@ -14,27 +35,4 @@ function toggleBackToTop() {
     } else {
         $("#btn_back_to_top").fadeOut();
     }
-}
-
-
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      //panel.style.display = "none";
-      $(panel).slideUp();
-    } else {
-      //panel.style.display = "block";
-      $(panel).slideDown();
-    }
-  });
 }
